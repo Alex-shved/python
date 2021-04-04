392a11d464a4a6174b6e891eae95ea71cb9bc6cf
a.shved <a.shved@corp.vk.com>
2021-04-05 00:02:56 +0300

    little fix

--- lesson-1/third.py
+++ lesson-1/third.py
@@ -1,2 +1,5 @@
+#3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3.
+# Считаем 3 + 33 + 333 = 369.
+
 number = input('Введите числа - ')
 print(int(number)+int(number+number)+int(number+number+number))
